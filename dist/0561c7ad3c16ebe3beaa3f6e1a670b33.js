f9d8af31f48af56bcdbd79848d29764a463761e8
ShAd0wInK <alexandredelaloy@live.com>
2018-03-07 17:53:34 +0100

    :construction: code HTML header

--- dist/0561c7ad3c16ebe3beaa3f6e1a670b33.js
+++ dist/0561c7ad3c16ebe3beaa3f6e1a670b33.js
@@ -150,9 +150,7 @@ module.exports = reloadCSS;
 require('flexboxgrid');
 
 require('./styles/styles.scss');
-
-document.write('JS\'s connected');
-},{"flexboxgrid":5,"./styles/styles.scss":3}],19:[function(require,module,exports) {
+},{"flexboxgrid":5,"./styles/styles.scss":3}],20:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -275,5 +273,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[19,2])
+},{}]},{},[20,2])
 //# sourceMappingURL=/dist/0561c7ad3c16ebe3beaa3f6e1a670b33.map
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 import 'flexboxgrid';
 import './styles/styles.scss';
-
-document.write('JS\'s connected');
